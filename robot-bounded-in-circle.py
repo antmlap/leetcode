c40d5b6e84302dbd1e1f6540c900275d6a2427ce
antmlap <antmlap@gmail.com>
2025-05-30 11:33:51 -0400

    Solved robot bounded in circle

--- robot-bounded-in-circle.py
+++ robot-bounded-in-circle.py
@@ -1,2 +1,34 @@
-print("Hello World!")
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        self.x = 0
+        self.y = 0
+        self.direction = 0 
+        def moveForward():
+            while self.direction < 0:
+                self.direction += 360
+            while self.direction >= 360:
+                self.direction -= 360
+            if self.direction ==0:
+                self.y+=1
+            elif self.direction == 90:
+                self.x +=1
+            elif self.direction == 180:
+                self.y -=1
+            elif self.direction == 270:
+                self.x -=1 
+            else:
+                print("Error")
+            
+        for instruction in instructions:
+            if instruction == "G":
+                moveForward()
+            elif instruction == "L":
+                self.direction -= 90
+            elif instruction == "R":
+                self.direction +=90
+            else:
+                print("Error")
+        return (self.x== 0 and self.y == 0) or self.direction % 360 != 0
+        
+
 
